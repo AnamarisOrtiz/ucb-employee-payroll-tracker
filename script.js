1144a6db409ed02bd4d26400496a3cb550e7db60
anamarisortiz <anamarisortiz@hotmail.com>
2024-06-28 20:28:02 -0400

    wrote getRandomEmployee function

--- script.js
+++ script.js
@@ -37,7 +37,11 @@ const displayAverageSalary = function(employeesArray) {
 // Select a random employee
 const getRandomEmployee = function(employeesArray) {
   // TODO: Select and display a random employee
+  const randomIndex = Math.floor(Math.random() * employeesArray.length);
+  const randomEmployee = employeesArray[randomIndex];
+  console.log(`Congratulations to ${randomEmployee.firstName} ${randomEmployee.lastName}, our random drawing winner!`);
 }
+
 
 /*
   ====================
